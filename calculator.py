85d68509ae24046dfa756886ce6ace49765ae48f
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2022-10-03 13:38:04 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,21 @@
 line = input("Enter an equation")
-arr=line.split(' ')
 
-while(arr.len()>0 and arr[0]==0){
-}
+while(arr.len()>0 and arr[0]==0):
+    arr=line.split(' ')
+    holder = arr[0]
+    for i in range(1,arr.len()):
+        if(i=='+'):
+            holder += arr[i+1]
+        elif(i=='-'):
+            holder -= arr[i+1]
+        elif(i=='*'):
+            holder *= arr[i+1]
+        elif(i=='/'):
+            holder /= arr[i+1]
+        else:
+            break
+        print("The outcome is "+ holder)
+        line = input("Enter an equation")
+
+print("peace")
+
